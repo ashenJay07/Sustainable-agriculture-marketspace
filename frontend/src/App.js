084960fdb8b1de812416a463520f37cd891f9356
Ashen Jayarathne <ashenj.official@gmail.com>
2023-05-10 07:20:21 +0530

    Complete all the implementations of Payment handling.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,22 +17,6 @@ import OrderSuccess from "./components/cart/OrderSuccess";
 // Auth or User imports
 import Login from "./components/user/Login";
 import Register from "./components/user/Register";
-import Profile from "./components/user/Profile";
-import UpdateProfile from "./components/user/UpdateProfile";
-import UpdatePassword from "./components/user/UpdatePassword";
-import ForgotPassword from "./components/user/ForgotPassword";
-import NewPassword from "./components/user/NewPassword";
-
-// Admin Imports
-import Dashboard from "./components/admin/Dashboard";
-import ProductsList from "./components/admin/ProductsList";
-import NewProduct from "./components/admin/NewProduct";
-import UpdateProduct from "./components/admin/UpdateProduct";
-import OrdersList from "./components/admin/OrdersList";
-import ProcessOrder from "./components/admin/ProcessOrder";
-import UsersList from "./components/admin/UsersList";
-import UpdateUser from "./components/admin/UpdateUser";
-import ProductReviews from "./components/admin/ProductReviews";
 
 import ProtectedRoute from "./components/route/ProtectedRoute";
 import { loadUser } from "./actions/userActions";
@@ -82,71 +66,7 @@ function App() {
 
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
-          <Route path="/password/forgot" component={ForgotPassword} exact />
-          <Route path="/password/reset/:token" component={NewPassword} exact />
-          <ProtectedRoute path="/me" component={Profile} exact />
-          <ProtectedRoute path="/me/update" component={UpdateProfile} exact />
-          <ProtectedRoute
-            path="/password/update"
-            component={UpdatePassword}
-            exact
-          />
         </div>
-
-        <ProtectedRoute
-          path="/dashboard"
-          isAdmin={true}
-          component={Dashboard}
-          exact
-        />
-        <ProtectedRoute
-          path="/admin/products"
-          isAdmin={true}
-          component={ProductsList}
-          exact
-        />
-        <ProtectedRoute
-          path="/admin/product"
-          isAdmin={true}
-          component={NewProduct}
-          exact
-        />
-        <ProtectedRoute
-          path="/admin/product/:id"
-          isAdmin={true}
-          component={UpdateProduct}
-          exact
-        />
-        <ProtectedRoute
-          path="/admin/orders"
-          isAdmin={true}
-          component={OrdersList}
-          exact
-        />
-        <ProtectedRoute
-          path="/admin/order/:id"
-          isAdmin={true}
-          component={ProcessOrder}
-          exact
-        />
-        <ProtectedRoute
-          path="/admin/users"
-          isAdmin={true}
-          component={UsersList}
-          exact
-        />
-        <ProtectedRoute
-          path="/admin/user/:id"
-          isAdmin={true}
-          component={UpdateUser}
-          exact
-        />
-        <ProtectedRoute
-          path="/admin/reviews"
-          isAdmin={true}
-          component={ProductReviews}
-          exact
-        />
 
         {!loading && (!isAuthenticated || user.role !== "admin") && <Footer />}
       </div>
--- frontend/src/components/product/ProductDetails.js
+++ frontend/src/components/product/ProductDetails.js
@@ -1,19 +1,12 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState } from "react";
 import { Carousel } from "react-bootstrap";
 
 import Loader from "../layout/Loader";
 import MetaData from "../layout/MetaData";
-import ListReviews from "../review/ListReviews";
 
 import { useAlert } from "react-alert";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  getProductDetails,
-  newReview,
-  clearErrors,
-} from "../../actions/productActions";
 import { addItemToCart } from "../../actions/cartActions";
-import { NEW_REVIEW_RESET } from "../../constants/productConstants";
 
 const ProductDetails = ({ match }) => {
   const [quantity, setQuantity] = useState(1);
@@ -27,28 +20,6 @@ const ProductDetails = ({ match }) => {
     (state) => state.productDetails
   );
   const { user } = useSelector((state) => state.auth);
-  const { error: reviewError, success } = useSelector(
-    (state) => state.newReview
-  );
-
-  useEffect(() => {
-    dispatch(getProductDetails(match.params.id));
-
-    if (error) {
-      alert.error(error);
-      dispatch(clearErrors());
-    }
-
-    if (reviewError) {
-      alert.error(reviewError);
-      dispatch(clearErrors());
-    }
-
-    if (success) {
-      alert.success("Reivew posted successfully");
-      dispatch({ type: NEW_REVIEW_RESET });
-    }
-  }, [dispatch, alert, error, reviewError, match.params.id, success]);
 
   const addToCart = () => {
     dispatch(addItemToCart(match.params.id, quantity));
@@ -73,52 +44,12 @@ const ProductDetails = ({ match }) => {
     setQuantity(qty);
   };
 
-  function setUserRatings() {
-    const stars = document.querySelectorAll(".star");
-
-    stars.forEach((star, index) => {
-      star.starValue = index + 1;
-
-      ["click", "mouseover", "mouseout"].forEach(function (e) {
-        star.addEventListener(e, showRatings);
-      });
-    });
-
-    function showRatings(e) {
-      stars.forEach((star, index) => {
-        if (e.type === "click") {
-          if (index < this.starValue) {
-            star.classList.add("orange");
-
-            setRating(this.starValue);
-          } else {
-            star.classList.remove("orange");
-          }
-        }
-
-        if (e.type === "mouseover") {
-          if (index < this.starValue) {
-            star.classList.add("yellow");
-          } else {
-            star.classList.remove("yellow");
-          }
-        }
-
-        if (e.type === "mouseout") {
-          star.classList.remove("yellow");
-        }
-      });
-    }
-  }
-
   const reviewHandler = () => {
     const formData = new FormData();
 
     formData.set("rating", rating);
     formData.set("comment", comment);
     formData.set("productId", match.params.id);
-
-    dispatch(newReview(formData));
   };
 
   return (
@@ -266,10 +197,6 @@ const ProductDetails = ({ match }) => {
               </div>
             </div>
           </div>
-
-          {product.reviews && product.reviews.length > 0 && (
-            <ListReviews reviews={product.reviews} />
-          )}
         </Fragment>
       )}
     </Fragment>
